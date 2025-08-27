110255ea855cf5ae297bc1b5f64f7e9552a61db6
ziggy-star-dust <71760203+ziggy-star-dust@users.noreply.github.com>
2025-08-27 10:41:03 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/slashhhash-static-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/slashhhash-static-website/' : '',
+  basePath:  '',
+  assetPrefix:  '',
   skipTrailingSlashRedirect: true,
   eslint: {
     ignoreDuringBuilds: true,
